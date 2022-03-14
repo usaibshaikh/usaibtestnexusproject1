cd0de770b4204285e8933e218d9a3379d78868a8
usaibshaikh <88020634+usaibshaikh@users.noreply.github.com>
2022-03-14 18:43:42 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package usaibtestnexusproject1.com.project.usaibtestnexusproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
